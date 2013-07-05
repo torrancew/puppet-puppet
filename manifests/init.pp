1dd9bbbd8b1d34b6e6fa9be0e3cd1a75dbee9435
Tray Torrance <devwork@warrentorrance.com>
2013-07-05 01:56:12 -0700

    Make master and use_puppetdb "module-level" parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,10 +23,22 @@
 # Copyright 2013, Tray Torrance
 # unless otherwise noted.
 #
-class puppet {
-  class { 'puppet::install': }
-  class { 'puppet::configure': }
-  class { 'puppet::service': }
+class puppet(
+  $master       = $puppet::params::master,
+  $use_puppetdb = $puppet::params::use_puppetdb
+) inherits puppet::params {
+  class {
+    'puppet::install':
+      master       => $master,
+      use_puppetdb => $use_puppetdb;
+
+    'puppet::configure':
+      master       => $master,
+      use_puppetdb => $use_puppetdb;
+
+    'puppet::service':
+      master => $master;
+  }
 
   anchor { 'puppet::begin': } -> Class['puppet::install']
   Class['puppet::service']    -> anchor { 'puppet::end': }
